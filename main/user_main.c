5e3f18cd10457ca48dd52e45c02e728796fd52ea
Gauthier Abomnes <gauwan@gmail.com>
2019-01-30 20:47:29 +0000

    Initialisation OK

--- main/user_main.c
+++ main/user_main.c
@@ -11,19 +11,40 @@
 #include "esp_system.h"
 
 #include "driver/gpio.h"
+#include <dht/dht.h>
+
+// LED_POWER_PIN    D1 GPIO_5
+// LED_WIFI_PIN     D2 GPIO_4
+// MOTOR_PIN        D4 GPIO_2
+
+// BUTTON_PIN       D5 GPIO_14
+// DHT11_PIN        D6 GPIO_12
 
 
 void initIO(void)
 {
-	gpio_config_t in;
-	in.mode=GPIO_MODE_INPUT;
-	in.pin_bit_mask = GPIO_Pin_4 | GPIO_Pin_5;
-	gpio_config(&in);
+	gpio_config_t configIO;
 
-	gpio_config_t out;
-        out.mode=GPIO_MODE_OUTPUT;
-       	out.pin_bit_mask = GPIO_Pin_4 | GPIO_Pin_5;
-        gpio_config(&out);
+    // Configuration OUT (LED | Motor)
+    configIO.mode=GPIO_MODE_OUTPUT;
+    configIO.pin_bit_mask = GPIO_Pin_2 | GPIO_Pin_4 | GPIO_Pin_5;
+    configIO.intr_type = GPIO_INTR_DISABLE;
+    configIO.pull_down_en = configIO.pull_up_en = 0;
+    gpio_config(&configIO);
+
+    // Configuration Button
+	configIO.mode=GPIO_MODE_INPUT;
+	configIO.pin_bit_mask = GPIO_Pin_14;
+    configIO.intr_type = GPIO_INTR_POSEDGE;
+    configIO.pull_up_en = 1;
+	gpio_config(&configIO);
+
+    // Configuration Temperature
+	configIO.mode=GPIO_MODE_INPUT;
+	configIO.pin_bit_mask = GPIO_Pin_12;
+    configIO.intr_type = GPIO_INTR_ANYEDGE; //?
+    configIO.pull_down_en = 1; //?
+	gpio_config(&configIO);
 }
 
 
@@ -36,6 +57,13 @@ void initIO(void)
 *******************************************************************************/
 void app_main(void)
 {
+    // Informations
+    printf("Bienvenue sur le Tempauto!");
     printf("SDK version:%s\n", esp_get_idf_version());
-    printf("Hello world!");
+
+    //Initialisation des IO
+    initIO();
+
+    // Allumer la LED ON/OFF
+    gpio_set_level(GPIO_NUM_0, 1);
 }
