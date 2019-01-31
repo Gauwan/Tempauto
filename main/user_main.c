fd4ce8bd1bf792566993e8d5af3acaad70c920da
Gauthier Abomnes <gauwan@gmail.com>
2019-01-31 14:40:10 +0000

    Basic usage OK

--- main/user_main.c
+++ main/user_main.c
@@ -10,8 +10,15 @@
 
 #include "esp_system.h"
 
+//#include <Servo.h>
+
 #include "driver/gpio.h"
-#include <dht/dht.h>
+#include "driver/adc.h"
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "driver/pwm.h"
 
 // LED_POWER_PIN    D1 GPIO_5
 // LED_WIFI_PIN     D2 GPIO_4
@@ -20,16 +27,32 @@
 // BUTTON_PIN       D5 GPIO_14
 // DHT11_PIN        D6 GPIO_12
 
+#define PWM_0_OUT_IO_NUM 2
 
+// Variable globale
+int16_t temperature = 20;
+int16_t selectedTemp;
+
+bool radiatorIsOpen = false;
+
+
+/******************************************************************************
+ * FunctionName : initIO
+ * Description  : Initialization of Input and Output
+ * Parameters   : none
+ * Returns      : none
+*******************************************************************************/
 void initIO(void)
 {
+    printf("Start initIO() ... ");
 	gpio_config_t configIO;
 
     // Configuration OUT (LED | Motor)
     configIO.mode=GPIO_MODE_OUTPUT;
     configIO.pin_bit_mask = GPIO_Pin_2 | GPIO_Pin_4 | GPIO_Pin_5;
     configIO.intr_type = GPIO_INTR_DISABLE;
-    configIO.pull_down_en = configIO.pull_up_en = 0;
+    configIO.pull_down_en = 0;
+    configIO.pull_up_en = 0;
     gpio_config(&configIO);
 
     // Configuration Button
@@ -40,13 +63,28 @@ void initIO(void)
 	gpio_config(&configIO);
 
     // Configuration Temperature
-	configIO.mode=GPIO_MODE_INPUT;
-	configIO.pin_bit_mask = GPIO_Pin_12;
-    configIO.intr_type = GPIO_INTR_ANYEDGE; //?
-    configIO.pull_down_en = 1; //?
+	configIO.mode=GPIO_MODE_OUTPUT_OD;
+	configIO.pin_bit_mask = GPIO_NUM_12;
+    configIO.intr_type = GPIO_INTR_DISABLE;
+    configIO.pull_down_en = 0;
+    configIO.pull_up_en = 0;
 	gpio_config(&configIO);
+
+    printf("finished!\n");
 }
 
+
+/******************************************************************************
+ * FunctionName : app_main
+ * Description  : entry of user application, init user function here
+ * Parameters   : none
+ * Returns      : none
+*******************************************************************************/
+void handleButton()
+{
+    //printf("Button pressed.\n");
+    radiatorIsOpen = 1-radiatorIsOpen;
+}
 
 
 /******************************************************************************
@@ -58,12 +96,82 @@ void initIO(void)
 void app_main(void)
 {
     // Informations
-    printf("Bienvenue sur le Tempauto!");
+    printf("Bienvenue sur le Tempauto!\n");
     printf("SDK version:%s\n", esp_get_idf_version());
 
-    //Initialisation des IO
+    // Initialisation des IO
     initIO();
+    
+    // Intialisation des handler
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(GPIO_NUM_14, handleButton, NULL);
+
+    // Initialisation moteur
+
+    const uint32_t pin_num[1] = {
+        PWM_0_OUT_IO_NUM
+    };
+
+    // dutys table, (duty/PERIOD)*depth
+    uint32_t duty[1] = {600};
+    int16_t phase[1] = {0};
+
+    pwm_init(20000, duty,1,pin_num);
+    pwm_set_phases(phase);
+
+    pwm_start();
+    
 
     // Allumer la LED ON/OFF
-    gpio_set_level(GPIO_NUM_0, 1);
+    gpio_set_level(GPIO_NUM_5, 1);
+
+    // Boucle principal
+    while(1)
+    {
+        //Choix de la temperature
+        if (selectedTemp != adc_read()/25)
+        {
+            selectedTemp = adc_read()/25;
+        }
+
+
+        if (radiatorIsOpen && ((selectedTemp-temperature) > 0))
+        {
+            uint32_t nDuty[1] = {600}; // Min Ouverture
+            pwm_set_duties(nDuty);
+            pwm_start();
+
+            // MAJ
+            radiatorIsOpen = 0;
+            gpio_set_level(GPIO_NUM_4, 0);
+        }
+        else if (!radiatorIsOpen && ((selectedTemp-temperature) < 0))
+        {
+            uint32_t nDuty[1] = {1000}; // Max ouverture
+            pwm_set_duties(nDuty);
+            pwm_start();
+
+            // MAJ
+            radiatorIsOpen = 1;
+            gpio_set_level(GPIO_NUM_4, 1);
+        }
+            
+
+        // Servo-Motor
+        //gpio_set_level(GPIO_NUM_2, val);
+
+        
+        //Affichage
+        printf("Temperature : %i°C / %i°C\n", temperature, selectedTemp);
+
+        // Ralentissement
+        int duration = pdMS_TO_TICKS(500);
+        vTaskDelay(duration);
+    }
+
+    // Arret des handler
+    gpio_isr_handler_remove(GPIO_NUM_14);
+
+    // Eteindre la LED ON/OFF
+    gpio_set_level(GPIO_NUM_5, 0);
 }
